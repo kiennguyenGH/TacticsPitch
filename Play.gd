da69d6388727bc255071f1539dc8aec9c5bc57b9
Zzpw3n <zzpw3n@gmail.com>
2024-02-18 21:11:09 -0800

    Main Menu UI Functionality

---
+++ Play.gd
@@ -0,0 +1,14 @@
+extends Button
+
+## play button
+func _on_pressed():
+	get_tree().change_scene_to_file("res://leveltest.tscn")
+
+## quit button
+func _on_quit_pressed():
+	get_tree().quit()
+
+## settings button
+func _on_settings_pressed():
+	pass # Replace with function body.
+## idk something will happen, will figure out later
