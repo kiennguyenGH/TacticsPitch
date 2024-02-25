88a144b92e3a4dfe780a09e309e6b232e60eadfa
Zzpw3n <zzpw3n@gmail.com>
2024-02-25 15:53:01 -0800

    Added Settings Buttons

--- Play.gd
+++ Play.gd
@@ -2,7 +2,7 @@ extends Button
 
 ## play button
 func _on_pressed():
-	get_tree().change_scene_to_file("res://leveltest.tscn")
+	get_tree().change_scene_to_file("res://isometric_test.tscn")
 
 ## quit button
 func _on_quit_pressed():
