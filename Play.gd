572fd45e5af6bd0173b45ecf326190f7f942315a
Dzermak <makdzer@gmail.com>
2024-02-27 12:46:09 -0800

    CharacterUI rough
    rough draft of UI

--- Play.gd
+++ Play.gd
@@ -4,6 +4,7 @@ extends Button
 func _on_pressed():
 	get_tree().change_scene_to_file("res://leveltest.tscn")
 
+
 ## quit button
 func _on_quit_pressed():
 	get_tree().quit()
@@ -12,3 +13,7 @@ func _on_quit_pressed():
 func _on_settings_pressed():
 	pass # Replace with function body.
 ## idk something will happen, will figure out later
+
+##test
+func _on_character_ui_test_pressed():
+	get_tree().change_scene_to_file("res://UiTest.tscn")
