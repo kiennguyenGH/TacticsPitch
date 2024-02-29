c339bc9986807600420458f88e1bb31ba41b650d
kiennguyenGH <ktnguyen4647@gmail.com>
2024-02-29 14:53:53 -0800

    prototype grid movement

--- unit.gd
+++ unit.gd
@@ -12,6 +12,8 @@ func _input(event):
 			var mouse_pos = get_global_mouse_position()
 			
 			var tile_mouse_pos = tile_map.local_to_map(mouse_pos)
+			var pos2 = tile_map.map_to_local(tile_mouse_pos)
 			print(mouse_pos)
 			print(tile_mouse_pos)
-			position = Vector2(mouse_pos.x, mouse_pos.y)
+			print(pos2)
+			position = Vector2(pos2.x, pos2.y-12)
