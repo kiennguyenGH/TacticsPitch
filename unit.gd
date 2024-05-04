cc3a6e3e04cdbacc1803ec56956e0bdf39ad613b
kiennguyenGH <ktnguyen4647@gmail.com>
2024-05-03 20:55:11 -0700

    AStar pathfinding now considers obstacles

--- unit.gd
+++ unit.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var tile_map : TileMap = $"../Isometric Tilemap"
 
 var index: int = 0
+var current_index: int = 0
 var units: Array = []
 var astar_grid : AStarGrid2D
 var current_id_path: Array[Vector2i]
@@ -16,6 +17,10 @@ func _ready():
 	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
 	astar_grid.update()
 	
+	for i in tile_map.get_used_cells(0):
+		if !tile_map.get_cell_tile_data(0, i).get_custom_data("walkable"):
+			astar_grid.set_point_solid(i)
+			
 	units = get_children()
 	
 	for i in units:
@@ -41,7 +46,7 @@ func _input(event):
 			#print(pos2)
 			#print(tile_map.get_surrounding_cells(tile_mouse_pos))
 			
-			if data and !is_occupied(Vector2(pos2.x, pos2.y)):
+			if data and !is_occupied(Vector2(pos2.x, pos2.y)) and !is_moving and tile_map.get_cell_tile_data(0, tile_mouse_pos).get_custom_data("walkable"):
 				
 				var id_path = astar_grid.get_id_path(tile_map.local_to_map(units[index].position), tile_mouse_pos).slice(1)
 				print(id_path)
@@ -51,11 +56,13 @@ func _input(event):
 					
 					
 				units[index].unfocus()
-				#if index == units.size()-1:
-					#index = 0
-				#else:
-					#index += 1
-				units[index].focus()
+				is_moving = true
+				current_index = index
+				if index == units.size()-1:
+					index = 0
+				else:
+					index += 1
+				
 			#print(units.size())
 			#print(index)
 			
@@ -68,18 +75,27 @@ func is_occupied(tile_position):
 
 func _physics_process(delta):
 	if current_id_path.is_empty():
+		is_moving = false
+		refresh_grid()
+		units[index].focus()
 		return
 	var target_position = tile_map.map_to_local(current_id_path.front())
-	if units[index].position.x < target_position.x and units[index].position.y < target_position.y:
-		units[index].set_animation("DownRight")
-	elif units[index].position.x > target_position.x and units[index].position.y > target_position.y:
-		units[index].set_animation("UpLeft")
-	elif units[index].position.x > target_position.x and units[index].position.y < target_position.y:
-		units[index].set_animation("DownLeft")
+	if units[current_index].position.x < target_position.x and units[current_index].position.y < target_position.y:
+		units[current_index].set_animation("DownRight")
+	elif units[current_index].position.x > target_position.x and units[current_index].position.y > target_position.y:
+		units[current_index].set_animation("UpLeft")
+	elif units[current_index].position.x > target_position.x and units[current_index].position.y < target_position.y:
+		units[current_index].set_animation("DownLeft")
 	else:
-		units[index].set_animation("UpRight")
-	units[index].position = units[index].position.move_toward(target_position, 2)
+		units[current_index].set_animation("UpRight")
+	units[current_index].position = units[current_index].position.move_toward(target_position, 2)
 	
-	if units[index].position == target_position:
+	if units[current_index].position == target_position:
 		current_id_path.pop_front()
 	
+func refresh_grid():
+	for i in tile_map.get_used_cells(0):
+		if is_occupied(tile_map.map_to_local(i)) or !tile_map.get_cell_tile_data(0, i).get_custom_data("walkable"):
+			astar_grid.set_point_solid(i)
+		else:
+			astar_grid.set_point_solid(i, false)
